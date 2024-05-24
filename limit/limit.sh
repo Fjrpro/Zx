222c200e0338b17c35d15098bbbed111fa0a5e4e
Fjrpro <fjr246125@gmail.com>
2024-05-24 11:37:25 +0700

    Update limit.sh

--- limit/limit.sh
+++ limit/limit.sh
@@ -1,4 +1,4 @@
-REPO="https://raw.githubusercontent.com/Jembudmerah/vps/main/"
+REPO="https://raw.githubusercontent.com/Fjrpro/Zx/main/"
 wget -q -O /etc/systemd/system/limitvmess.service "${REPO}limit/limitvmess.service" && chmod +x limitvmess.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limitvless.service "${REPO}limit/limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limittrojan.service "${REPO}limit/limittrojan.service" && chmod +x limittrojan.service >/dev/null 2>&1
